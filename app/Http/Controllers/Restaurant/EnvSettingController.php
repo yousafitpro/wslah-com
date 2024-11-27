e0d5788bc9c533d7381b99c4f9290d7a027454d4
Muhammad Yousaf <yousaf.itpro@gmail.com>
2024-11-27 11:15:11 +0500

    asdasd

--- app/Http/Controllers/Restaurant/EnvSettingController.php
+++ app/Http/Controllers/Restaurant/EnvSettingController.php
@@ -47,7 +47,7 @@ class EnvSettingController extends Controller {
         // } else
         // {
             $restaurant = $user->restaurant;
-            $fb_user=json_decode($restaurant,true);
+            $fb_user=json_decode($restaurant->fb_user,true);
             dd($fb_user);
             return view('restaurant.settings.instagram', [
                 'row' => $restaurant,
