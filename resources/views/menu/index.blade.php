d26c783801193639602abfbf58d21bf35dce0350
Muhammad Yousaf <yousaf.itpro@gmail.com>
2024-12-10 02:40:05 +0500

    adasd

--- resources/views/menu/index.blade.php
+++ resources/views/menu/index.blade.php
@@ -2,6 +2,7 @@
 <html>
 
 <head>
+
     <title>{{ config('app.name') }}</title>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width,initial-scale=1">
@@ -210,6 +211,10 @@
 
 <body>
     <main style="">
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
         <input type="hidden" name="" id="is_vertical" value="{{ $is_vertical }}">
         <input type="hidden" id="animation_timer" value="{{ $animation_timer }}">
         @if ($is_vertical == 1)
